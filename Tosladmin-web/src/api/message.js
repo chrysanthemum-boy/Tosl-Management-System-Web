8c61198bc44419601c12ab9a8c47c20231fd272f
chrysanthemum-boy <1447051860@qq.com>
2022-03-08 19:49:12 +0800

    Tosl后台管理系统消息管理bug修正

--- Tosladmin-web/src/api/message.js
+++ Tosladmin-web/src/api/message.js
@@ -2,10 +2,38 @@ import request from '@/utils/request'
 
 export function getMessage(id) {
   return request({
-    url: 'message/all',
+    url: 'http://localhost:8000/message/all',
     method: 'get',
     params: {
       id: id
     }
+  })
+}
+
+export function loadMessage() {
+  return request({
+    url: 'http://localhost:8000/message',
+    method: 'get'
+  })
+}
+
+export function setMessage(message_id, user_id, user_name, message, send_to) {
+  return request({
+    url: 'https://106.14.35.94:8000/message/set',
+    method: 'post',
+    data: {
+      message_id,
+      user_id,
+      user_name,
+      message,
+      send_to
+    }
+  })
+}
+
+export function delAllMessage() {
+  return request({
+    url: 'http://localhost:8000/message/del',
+    method: 'delete'
   })
 }
